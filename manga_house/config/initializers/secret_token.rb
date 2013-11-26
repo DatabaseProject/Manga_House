582e7a8685a1b393e0804c3a5b35b51a4061e715
Jaktip <WinsukeRabbit@gmail.com>
2013-11-26 16:34:42 +0700

    Project configuration

---
+++ manga_house/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MangaHouse::Application.config.secret_key_base = 'dcf32c2f4406993936dab718a1a1e14a028e658307f55c4ee24daf4627ce2850184530d4605bb2ee98462bcda806221fd0c2bde8f32f72dc186efd387967fa5d'
